9106e15e82abd955ab76df4be134ed7a3c5d54ec
geehhyy <devkhun409@gmail.com>
2025-11-10 21:09:49 +0900

    3개씩 랜덤으로 나오되, 3개가 한번씩은 나오게

--- src/main/java/org/example/UniqueIdGenerator.java
+++ src/main/java/org/example/UniqueIdGenerator.java
@@ -1,10 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class UniqueIdGenerator {
+    private static final AtomicInteger counter = new AtomicInteger(0);
+    private static final List<Character> SUFFIX_POOL = Arrays.asList('A', 'B', 'C');
+    private static List<Character> currentSet = new ArrayList<>(SUFFIX_POOL);
+    private static int index = 0;
+
+    public static synchronized String nextId() {
+        // 세트가 끝났으면 새로 셔플
+        if (index == 0) {
+            Collections.shuffle(currentSet);
+        }
+
+        int baseId = counter.getAndIncrement();
+        char suffix = currentSet.get(index);
+
+        index = (index + 1) % SUFFIX_POOL.size();
+
+        return baseId + "-" + suffix;
+    }
+
     public static void main(String[] args) {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         AtomicInteger uniqueId = new AtomicInteger(0);
